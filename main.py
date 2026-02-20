2501b6dcc6e9b623b94f8eb99ee8e880709e653e
KnoVz <h@knovz.com>
2026-02-20 20:26:19 +0100

    cleaner output. Handle Sharepoint connection error

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Minimal app to test Sharepoint access from python
 """
 
 import os
+import sys
 import logging
 import logging.config
 import yaml
@@ -12,6 +13,14 @@ from restclients import SharepointClient
 
 logger = logging.getLogger(__name__)
 load_dotenv()
+
+
+def clear_screen() -> None:
+    """
+    Send clear screen cli command depending on Operating system.
+    https://stackoverflow.com/a/684344/4220807
+    """
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 def print_error(error):
@@ -30,17 +39,22 @@ def main() -> None:
     """
     Main entry point
     """
+    clear_screen()
+
     with open("logging_config.yaml", "r", encoding="utf-8") as file:
         log_conf = yaml.safe_load(file)
-
     logging.config.dictConfig(log_conf)
+
     logger.info("Running")
 
-    sc = SharepointClient(
-        os.getenv("SHAREPOINT_CLIENT_ID"),
-        os.getenv("SHAREPOINT_CLIENT_SECRET"),
-        os.getenv("SHAREPOINT_TENANT_ID"),
-    )
+    try:
+        sc = SharepointClient(
+            os.getenv("SHAREPOINT_CLIENT_ID"),
+            os.getenv("SHAREPOINT_CLIENT_SECRET"),
+            os.getenv("SHAREPOINT_TENANT_ID"),
+        )
+    except ConnectionError as ce:
+        sys.exit(f"Could not connect to Sharepoint. {ce}")
 
     logger.info(sc.app)
 
@@ -51,20 +65,6 @@ def main() -> None:
 
     site = sc.get_site_by_name("captarvision.sharepoint.com", "ShareTest")
     print(f"{site["displayName"]} - {site["id"]}")
-
-    # Works with both the long and the short version
-    # site_id = "captarvision.sharepoint.com,02d9aeff-b1b8-40dd-ad98-cfb57a6953af,1850c9c8-fb1e-4b87-90ab-322c010209d8"
-    # site_id = "02d9aeff-b1b8-40dd-ad98-cfb57a6953af"
-    site_id = (
-        "captarvision.sharepoint.com,"
-        "02d9aeff-b1b8-40dd-ad98-cfb57a6953af,"
-        "1850c9c8-fb1e-4b87-90ab-322c010209d8"
-    )
-    site = sc.get_site_by_id(site_id)
-    if "error" in site:
-        print_error(site["error"])
-    else:
-        print(f"{site["displayName"]} - {site["id"]}")
 
 
 if __name__ == "__main__":
